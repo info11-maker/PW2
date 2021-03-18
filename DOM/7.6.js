22a3f4bbe0909a09882eb080f26d9addfbdd6257
Mihalachi Alexandrina <54944618+info11-maker@users.noreply.github.com>
2021-03-18 13:33:26 +0200

    Add files via upload

---
+++ DOM/7.6.js
@@ -0,0 +1,12 @@
+var t = document.getElementById("board").innerHTML;
+var row1 = t.childNode[0];
+var row2 = t.childNode[1];
+var row3 = t.childNode[2];
+var row4 = t.childNode[3];
+var row5 = t.childNode[4];
+var row6 = t.childNode[5];
+var row7 = t.childNode[6];
+var row8 = t.childNode[7];
+          
+var W1 = document.getElementById("board").row[1].cell[2];
+
