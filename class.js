6bea52ccdebd3b5ee59a081e396270e996e8ccd5
Mihalachi Alexandrina <54944618+info11-maker@users.noreply.github.com>
2021-03-25 13:24:42 +0200

    Objects

---
+++ class.js
@@ -0,0 +1,62 @@
+/*class Person{
+    constructor(firstName, surName){
+        this.firstName = firstName;
+        this.surName = surName;
+    }
+}
+var worker = new Person("Ivan", "Ivanov");
+console.log(worker.firstName);
+console.log(worker.surName);*/
+
+/*class Person{
+  constructor(Name, surName){
+      this.Name = Name;
+      this.surName = surName;
+  }
+}
+var worker = new Person("Ivan", "Ivanov");
+console.log(worker.getName);
+console.log(worker.getsurName);
+console.log(worker.setsurName("Petrov"));
+console.log(worker.getsurName);*/
+
+
+/*class Validator {
+  constructor(Email, Date) {
+    this.Email = Email;
+    this.Date = Date;
+  }
+  
+  static isEmail() {
+    var txt;
+    if (validator === null || validator === "") {
+      txt = "false";
+    } else {
+      txt = "true";
+    }
+}
+}
+var validator = prompt ("please enter your mail:", "my@mail.ru");*/
+
+/*class User{
+  constructor(Name,surName, year){
+      this.Name = Name;
+      this.surName = surName;
+      this.year = year;
+  }
+}
+
+class Student extends User{
+  constructor(){
+      super();
+      this.Name = Name;
+      this.surName = surName;
+      this.year = year;
+  }
+}
+const user = new User("Ivan", "Ivanov", 2018);
+console.log(user.Name);
+console.log(user.surName);
+console.log(user.year);*/
+
+
